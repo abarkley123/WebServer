023421eb41fe4f5a14c431e3b4b9cf9cae4d2774
abarkley123 <Barkley36@gmail.com>
2019-07-08 21:46:24 +0100

    feature/MainBodyImpl: Adding basic routing to handler.

--- main.go
+++ main.go
@@ -22,12 +22,20 @@ func MainHandler() http.Handler {
 	})
 }
 
+func PortfolioHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "./templates/portfolio.html", nil)
+	})
+}
+
 func main() {
     r := mux.NewRouter()
     r.Handle("/", MainHandler()).Methods("GET")
+    r.Handle("/portfolio.html", PortfolioHandler()).Methods("GET")
 
     fs := http.FileServer(http.Dir("./static"))
     http.Handle("/", r)
+    http.Handle("/portfolio.html", r)
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe(":8080", nil)
 }
