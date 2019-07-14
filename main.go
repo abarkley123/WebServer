0c2e1b705f6ee7eaa4e727f0595a7e6ba72105c7
abarkley123 <Barkley36@gmail.com>
2019-07-14 11:06:28 +0100

    feature/MainBodyImpl: Routing changes.

--- main.go
+++ main.go
@@ -28,14 +28,30 @@ func PortfolioHandler() http.Handler {
 	})
 }
 
+func AboutHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "./templates/about.html", nil)
+	})   
+}
+
+func ContactHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "./templates/contact.html", nil)
+	})   
+}
+
 func main() {
     r := mux.NewRouter()
     r.Handle("/", MainHandler()).Methods("GET")
     r.Handle("/portfolio.html", PortfolioHandler()).Methods("GET")
+    r.Handle("/about.html", AboutHandler()).Methods("GET")
+    r.Handle("/contact.html", ContactHandler()).Methods("GET")
 
     fs := http.FileServer(http.Dir("./static"))
     http.Handle("/", r)
     http.Handle("/portfolio.html", r)
+    http.Handle("/about.html", r)
+    http.Handle("/contact.html", r)
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe(":8080", nil)
 }
